01d574c89ef52b1b48895e17a7f442b49720ce09
alanChibilisco <alan.chibilisco@gmail.com>
2021-10-04 17:47:01 -0300

    modifique el ejercicio 10

--- js/ej10.js
+++ js/ej10.js
@@ -5,11 +5,17 @@ let columnas=parseInt(prompt("Ingrese el numero de columnas")
 
 let nroInicio=fila*columnas;
 
+document.write("<table border>");
+
 for(let i=1; i<=fila; i++){
-    document.write("<br><br>Fila N°"+i+"--:")
+    document.write("<tr>")
     for(let j=1; j<=columnas; j++){
-        document.write("  ");
-        document.write(nroInicio)
+        document.write("<td>"+nroInicio+"</td>");
+        /*document.write(nroInicio)*/
         nroInicio--;
     }
+    document.write("</tr>");
 }
+
+
+document.write("</table>");
